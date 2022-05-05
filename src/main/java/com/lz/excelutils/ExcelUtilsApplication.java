8716012112c0136460d1ca2d40e0ded973a9fddb
lz <903335848@qq.com>
2022-05-05 14:06:19 +0800

    springBoot相关

---
+++ src/main/java/com/lz/excelutils/ExcelUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.lz.excelutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelUtilsApplication.class, args);
+	}
+
+}
